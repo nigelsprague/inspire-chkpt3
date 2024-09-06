20369f046ac6e37c74f7e96e1fb1524fdaabd56c
Nigel Sprague <spraguenigel27@gmail.com>
2024-09-06 12:12:14 -0600

    initial

---
+++ app/App.js
@@ -0,0 +1,23 @@
+import { AuthController } from './controllers/AuthController.js';
+import { ToDoController } from './controllers/ToDoController.js';
+import { router } from './router-config.js';
+ToDoController
+const USE_ROUTER = false
+
+class App {
+
+  AuthController = new AuthController()
+  ToDoController = new ToDoController()
+
+  constructor() {
+    if (USE_ROUTER) {
+      this.router = router
+      this.router.init(this)
+    }
+  }
+}
+
+
+const app = new App()
+// @ts-ignore
+window.app = app
---
+++ app/controllers/ToDoController.js
@@ -0,0 +1,5 @@
+export class ToDoController {
+  constructor() {
+    console.log('ToDo loaded')
+  }
+}
